b7cad964522d48b9b1f6b566809b66489dbacdca
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-11-04 13:13:11 -0600

    create getCourseCodes

--- server/webscraping/webscrape.py
+++ server/webscraping/webscrape.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import re
 
 
+catalogue_link = "https://catalogue.usask.ca/"
 # api call to get the content of the usask catalogue page
-catalogue = requests.get("https://catalogue.usask.ca/")
+catalogue = requests.get(catalogue_link)
+# scrape the webpage with beautiful soup library
 webpage = bs(catalogue.content)
 
 # get the course codes from the webpage
@@ -18,5 +21,30 @@ def getSubjectCodes():
     subject_code_refined.pop(0)
     return subject_code_refined
 
+'''
+gets all the course codes for a particular subject code 
+'''
+def getCourseCodes(subject_code):
+    # api call to get the content of the catalogie for a specific subject e.g CMPT
+    course_weblink = catalogue_link+"?subj_code="+str(subject_code)+"&cnum="
+    course_catalogue = requests.get(course_weblink)
+    course_webpage = bs(course_catalogue.content)
+    course = course_webpage.select("div h4 a")
+    course_name = [str(c.find(string=re.compile(str(subject_code)))).strip() for c in course]
+    course_name = [str(c).split(".", 1)[0] for c in course_name]
+    return(course_name) 
 
-print(getSubjectCodes())
+def main():
+    # test cases for each method
+
+    # test case for getSubjectCodes
+    getSubjectCodes()
+
+    # test case for getCourseCodes
+
+list_of_subjects = getSubjectCodes() 
+# print(list_of_subjects[3])
+# print(getCourseCodes(list_of_subjects[3]))
+
+for course in list_of_subjects:
+    print(getCourseCodes(course))
