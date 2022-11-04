e368d68ce5625c6a7c73f8f86e2ed6de0c5999be
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-11-03 19:30:24 -0600

    refactor getCourseCode to getSubjectCodes

--- server/webscraping/webscrape.py
+++ server/webscraping/webscrape.py
@@ -8,15 +8,15 @@ webpage = bs(catalogue.content)
 
 # get the course codes from the webpage
 
-# function that returns the a list of course codes from the webpage.
-def getCourseCode():
-    # get the course codes from the webpage
-    course_code_raw = webpage.select('option[value]')
-    course_code = [c.get('value') for c in course_code_raw]
-    course_code1 = [c for c in course_code]
-    course_code2 = [str(c).split(" ", 1)[0] for c in course_code1]
-    course_code2.pop(0)
-    return course_code2
+# function that returns the a list of subject codes from the webpage.
+def getSubjectCodes():
+    # get the csubject codes from the webpage
+    subject_code_raw = webpage.select('option[value]')
+    subject_code = [subject.get('value') for subject in subject_code_raw]
+    subject_code1 = [subject for subject in subject_code]
+    subject_code_refined = [str(c).split(" ", 1)[0] for c in subject_code1]
+    subject_code_refined.pop(0)
+    return subject_code_refined
 
 
-print(getCourseCode())
+print(getSubjectCodes())
