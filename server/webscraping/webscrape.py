3aa71c88ad21801db5f65381c9c81dffb53fc305
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-11-05 16:00:49 -0600

    create getCoursePrerequisites

--- server/webscraping/webscrape.py
+++ server/webscraping/webscrape.py
@@ -75,6 +75,42 @@ def getCourseLinks(subject_code):
     return course_links
 
 
+# course_1 = web.find_all("b", string=re.compile("Pre"))
+# for course in course_1:
+#     course = course_1.find("b", string=re.compile("Pre"))
+#     course1 = course.next_element.next_element
+#     print(course1)
+# r = requests.get("https://catalogue.usask.ca/?subj_code=CMPT&cnum=")
+# web = bs(r.content)
+# course_1_1 = web.select("div.col-md-5 p")
+# for c in course_1_1:
+#    c1 =  c.find("b", string=re.compile("Pre"))
+#    print(c1)
+#    if c1 == None:
+#     c2 = "none"
+#     print(c2)
+#    else:
+#     c2 = c1.next_element.next_element
+#     print(c2)
+# course_1_2 = [c.find("b", string=re.compile("Pre")) for c in course_1_1]
+# course_1_3 = [c.next_element.next_element for c in course_1_2]
+# print(course_1_2)
+
+def getCoursePrerequisites(subject_code):
+    course_weblink = catalogue_link+"?subj_code="+str(subject_code)+"&cnum="
+    course_catalogue = requests.get(course_weblink)
+    course_webpage = bs(course_catalogue.content)
+    course = course_webpage.select("div.col-md-5 p")
+    course_preq_list = []
+    for c in course:
+        c1 =  c.find("b", string=re.compile("Pre"))
+        if c1 == None:
+            course_preq_list.append("None")
+        else:
+            course_preq_list.append(c1.next_element.next_element)
+    return course_preq_list
+            
+
 def main():
     # test cases for each method
 
@@ -91,4 +127,5 @@ list_of_subjects = getSubjectCodes()
 #     print(getCourseNames(course))
 
 # print(getCourseCodes('CMPT'))
-print(getCourseLinks('CMPT'))
+# print(getCourseLinks('CMPT'))
+# print(getCoursePrerequisites('CMPT'))
