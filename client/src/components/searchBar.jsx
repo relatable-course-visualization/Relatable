975a95e2846589d8b6fc95eb01b05ab984c3dfee
Christophe <chg502@usask.ca>
2022-11-04 20:37:11 -0600

    searchable json searchbox

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -1,12 +1,114 @@
-const Search = () =>  {
-    return (
-        <div style={{display:'flex', justifyContent:"flex-end"}}>
-            <input type="text" placeholder="Search..."/>
+import '../stylings/searchBar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+
+function SearchBar({placeholder, data})  {
+
+    const [filteredData, setFilteredData] = useState([])
+    const [wordEntered, setWordEntered] = useState("")
+
+    const handleFilter = (event) =>{
+        const searchWord = event.target.value
+        setWordEntered(searchWord)
+        const newFilter = data.filter((value)=> {
+            return value.class_name.toLowerCase().includes(searchWord.toLowerCase())
+        })
+
+        if(searchWord===""){
+            setFilteredData([])
+        } else{
+            setFilteredData(newFilter)
+        }
+    }
+
+    const clearSearch = () =>{
+        setFilteredData([])
+        setWordEntered("")
+    }
+
+    return ( 
+        <div className="search">
+            <div className="searchInputs">
+                <input type="text" placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
+                <div className="searchIcon">
+                    {filteredData.length === 0 ? (
+                        <SearchIcon/>
+                    ) : (
+                        <CloseIcon id="clearBtn" onClick =  {clearSearch}/>
+                    )}
+                </div>
+            </div>
+
+            {filteredData.length !==0 && (
+                <div className="dataResult">
+                    {filteredData.slice(0,15).map((value,key)=>{
+                        return (<a className='dataItem'> 
+                            <p>{value.class_name}</p>
+                    
+                            </a> 
+                        )})}
+                </div>
+            )}
         </div>
-        
-    )
     
-    
+    )    
 }
 
-export default Search
+
+// function SearchBar({ placeholder, data }) {
+//     const [filteredData, setFilteredData] = useState([]);
+//     const [wordEntered, setWordEntered] = useState("");
+  
+//     const handleFilter = (event) => {
+//       const searchWord = event.target.value;
+//       setWordEntered(searchWord);
+//       const newFilter = data.filter((value) => {
+//         return value.class_name.toLowerCase().includes(searchWord.toLowerCase());
+//       });
+  
+//       if (searchWord === "") {
+//         setFilteredData([]);
+//       } else {
+//         setFilteredData(newFilter);
+//       }
+//     };
+  
+//     const clearInput = () => {
+//       setFilteredData([]);
+//       setWordEntered("");
+//     };
+  
+//     return (
+//       <div className="search">
+//         <div className="searchInputs">
+//           <input
+//             type="text"
+//             placeholder={placeholder}
+//             value={wordEntered}
+//             onChange={handleFilter}
+//           />
+//           <div className="searchIcon">
+//             {filteredData.length === 0 ? (
+//               <SearchIcon />
+//             ) : (
+//               <CloseIcon id="clearBtn" onClick={clearInput} />
+//             )}
+//           </div>
+//         </div>
+//         {filteredData.length != 0 && (
+//           <div className="dataResult">
+//             {filteredData.slice(0, 15).map((value, key) => {
+//               return (
+//                 <a className="dataItem" href={value.link} target="_blank">
+//                   <p>{value.class_name} </p>
+//                 </a>
+//               );
+//             })}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+  
+  export default SearchBar;
