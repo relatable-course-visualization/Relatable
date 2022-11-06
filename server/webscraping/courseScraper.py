a36409ec51d560fc83f073a3f3aca78d7a1716a4
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-11-06 13:33:05 -0600

    create preqCleaner

--- server/webscraping/courseScraper.py
+++ server/webscraping/courseScraper.py
@@ -9,4 +9,15 @@ def createCourse(code, name, desc, preq, restriction,link ):
     new_course = Course(code, name, desc, preq, restriction, link)
     return new_course
 
+'''
+helper function that cleans the prequisite strings
+'''
+def preqCleaner(prerequisite):
+    preq_list = courseCleaner(initialClean(prerequisite))
+    return preq_list
 
+# lis = getCoursePrerequisites('CMPT')[8]
+# lis1 = courseCleaner(initialClean(lis))
+
+# lis1 = courseCleaner(initialClean(getCoursePrerequisites('CMPT')[8]))
+# print(lis1)
