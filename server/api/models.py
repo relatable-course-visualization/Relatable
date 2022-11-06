10d35e61d08514e543cfc1ed158a252cca106127
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-06 14:30:06 -0600

    create and migrate prerequisite table model

--- server/api/models.py
+++ server/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Course table. See documentation on /database/database_documentation.md
+# For all tables, see documentation on /database/database_documentation.md
+
+# Course table
 class Course(models.Model):
     course_code = models.CharField(max_length=10, null=True)
     name = models.CharField(max_length=255, null=True)
@@ -8,3 +10,9 @@ class Course(models.Model):
     restrictions = models.TextField(null=True)
     hyperlink = models.CharField(max_length=255, null=True)
     num_credits = models.IntegerField(null=True)
+
+# Prerequisite table
+class Prerequisite(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_id')
+    conjunction_expression = models.CharField(max_length=1, null=True)
+    course_id_prereq = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_id_prereq')
