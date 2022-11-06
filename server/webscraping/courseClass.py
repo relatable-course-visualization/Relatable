da41870582c467039331f35c3cf466f3673b402e
Inioluwa Olaleye (ido030) <ido030@usask.ca>
2022-11-05 23:14:09 -0600

    create getCode

--- server/webscraping/courseClass.py
+++ server/webscraping/courseClass.py
@@ -3,10 +3,15 @@
 
 class Course:
     def __init__(self, code, name, description, prerequisite, restricition, link):
-        self.name = name
         self.code = code
+        self.name = name
         self.description = description
         self.prerequisite = prerequisite
         self.restriction = restricition
         self.link = link
+
+    def getCode(self):
+        return '{}'.format(self.code)
+    
+
 
