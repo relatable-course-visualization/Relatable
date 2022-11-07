d55df58a3f0219ae404393b9f31426a0a3760647
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-07 16:31:33 -0600

    add route for getting all prereqs

--- server/api/urls.py
+++ server/api/urls.py
@@ -5,13 +5,14 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls), 
 
-    # Client endpoints
+    # GET endpoints
     path('courses', views.getAllCourses), 
     re_path(r'^getCourse/([A-za-z]{2,4}\d{2,3})$', views.getCourse), #path like /getCourse/CMPT141/
     re_path(r'^getPrereqs/([A-za-z]{2,4}\d{2,3})$', views.getPrereqs), 
     re_path(r'^getDependants/([A-za-z]{2,4}\d{2,3})$', views.getDependants),
+    path('prerequisites', views.getAllPrereqs),
 
-    # Database endpoints
+    # POST endpoints
     path('postCourse', views.postCourse),
     path('postPrerequisite', views.postPrerequisite),
     path('postDependency', views.postDependency)
