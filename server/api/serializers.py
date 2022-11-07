6187f22199282a81426d76201dd686d40502fe5b
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-07 14:11:19 -0600

    write serializer for prerequisite

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from .models import Course
+from .models import *
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'course_code', 'name', 'description', 'restrictions', 'hyperlink', 'num_credits']
 
+class PrerequisiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prerequisite
+        fields = ['id', 'course_id', 'conjunction_expression', 'course_id_prereq']
