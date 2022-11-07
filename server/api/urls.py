4110af86350cfc58f229ec1b121ee5b69789c17f
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-06 19:16:18 -0600

    develop interal api function

--- server/api/urls.py
+++ server/api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     re_path(r'^getCourse/([A-za-z]{2,4}\d{2,3})/$', views.getCourse), #path like /getCourse/CMPT141/
     re_path(r'^getPrereqs/([A-za-z]{2,4}\d{2,3})/$', views.getPrereqs), 
     re_path(r'^getDependants/([A-za-z]{2,4}\d{2,3})/$', views.getDependants),
+
+    path('postCourses', views.postAllCourses)
 ]
