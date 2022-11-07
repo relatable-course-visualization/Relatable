028d3dc34c815aee4dd60b7d0979307b20157f0f
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-06 19:14:47 -0600

    update Course serializer

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -4,4 +4,5 @@ from .models import Course
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ('id', 'course_id', 'description')
+        fields = ['id', 'course_code', 'name', 'description', 'restrictions', 'hyperlink', 'num_credits']
+
