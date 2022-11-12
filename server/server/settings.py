65b2cddcc9a76e1b75dff569944d5f37c230f0f4
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-12 10:21:59 -0600

    add cors policy to whitelist client

--- server/server/settings.py
+++ server/server/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework',
     'server',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'server.urls'
