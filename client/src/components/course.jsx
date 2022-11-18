7575b432de0bc735f6508ed7669b57b9c1bbcbe8
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-11-17 21:11:44 -0600

    capture dependencies data; create relevant vars

--- client/src/components/course.jsx
+++ client/src/components/course.jsx
@@ -1,8 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Button from '@material-ui/core/Button'
 import "../stylings/course.css";
-import NewSearch from "./newSearch";
-
+import axios from 'axios';
 
 const Course = (props) => {
     
@@ -11,11 +10,30 @@ const Course = (props) => {
         return {course}
     }
 
-    console.log(props);
+    const [dependencies, setDependencies] = useState([]);
+    const [isDependenciesEmpty, setIsDependenciesEmpty] = useState(false);
+
+    var code = props.course_code.replace(" ", "");
+
+    useEffect(() => {
+        const loadDependency = (code) => {
+
+            // list of dependencies 
+            axios.get(
+            `${process.env.REACT_APP_SERVER_ENDPOINT}/getDependants/${code}`
+            ).then((response) => {
+                
+                var data = response.data;
+            })
+        }
+    
+        loadDependency(code);
+    }, []);
+    
     return(
         <div className="wrapper">
             <div className="course">  
-                <div className="course__title" >{props.title}</div>
+                <div className="course__title" >{props.course_code}</div>
                 <div className="course__body">{props.body}</div>
                 <div className="sub">  
                     <div className="course__subboxes">Prerequisites</div>
@@ -26,9 +44,7 @@ const Course = (props) => {
 
                 <div className="sub"> 
                     <div className="course__subboxes">Dependencies</div>
-                        <Button variant="contained"  onClick={(e) => courseHandler( e.currentTarget.innerText )}>
-                            {props.id}
-                        </Button>
+                            {isDependenciesEmpty ? <h2>No Dependencies</h2> : <h2>{dependencies}</h2>}        
                 </div>
                 <div className="sub">Placeholder for Restrictions </div>
                     <div className="course_body" >
