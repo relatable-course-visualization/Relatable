dcc204eb9048b0dc583505507107a9f8bfef6346
Fraser McLeod <fdm360@mail.usask.ca>
2022-11-19 16:43:17 -0600

    fix views

--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,10 @@
+from http.client import HTTPResponse
+import json
 from urllib import response
 from django.shortcuts import render
 from rest_framework import status
-
+import requests
+import environ
 # imports for API views
 from rest_framework import generics #View
 from rest_framework.views import APIView #APIView
@@ -9,14 +12,14 @@ from rest_framework.response import Response #APIView
 from rest_framework.decorators import api_view
 from .models import Course
 from .serializers import *
+# setting up environmental variables
+env = environ.Env()
+environ.Env.read_env()
 
 
 @api_view(['POST'])
 def postCourse(request):
     """ Insert course object into Course table
-        :param none
-        :postcond: course table has a new entry
-        :return: Response - either successfully posted or bad request
     """
     if request.method == 'POST':
         serializer = CourseSerializer(data=request.data)
@@ -24,13 +27,9 @@ def postCourse(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['POST'])
 def postPrerequisite(request):
     """ Insert prerequisite entry into Prerequisite table 
-        :param none
-        :postcond: prerequisite table has a new entry
-        :return: Response - either successfully posted or bad request
     """
     if request.method == 'POST':
         serializer = PrerequisiteSerializer(data=request.data)
@@ -38,13 +37,9 @@ def postPrerequisite(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['POST'])
 def postDependency(request):
     """ Insert dependency entry into Depedency table 
-        :param none
-        :postcond: dependancy table has a new entry
-        :return: Response - either successfully posted or bad request
     """
     if request.method == 'POST':
         serializer = DependencySerializer(data=request.data)
@@ -52,6 +47,7 @@ def postDependency(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['PUT'])
 def updateCourse(request):
@@ -78,8 +74,8 @@ def updateCourse(request):
 @api_view(['GET'])
 def getAllCourses(request):
     """ Return all course objects from DB
-        :param none
-        :return: Response - all course objects serialized
+        :param: none
+        :return: serialized course objects OR serialized course ids depending on need
     """
     courses = Course.objects.all()
     serializer = CourseSerializer(courses, many=True)
@@ -88,17 +84,12 @@ def getAllCourses(request):
 
 @api_view(['GET'])
 def getCourse(request, course_code):
-    """ Return a single course, given the ID if possible
-        :param course_code: the course code of the course that should be retrieved (note expect no space)
-        :return: Response - a serialized course object
-    """
-
+    """ Return a single course, given the ID if possible"""
     #add space to course code
     course_code_with_space = list(course_code)
     course_code_with_space.insert(-3," ")
     course_code_with_space = ''.join(course_code_with_space)
     course_code_with_space = str(course_code_with_space)
-
     try:
         course = Course.objects.get(course_code=course_code_with_space)
     except:
@@ -107,20 +98,20 @@ def getCourse(request, course_code):
     
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getAllPrereqs(request):
     """ Return all prerequisite objects from DB
         :param: none
-        :return: serialized Prerequisite object
+        :return: serialized Prerequisite objects
     """
     prerequisites = Prerequisite.objects.all()
     serializer = PrerequisiteSerializer(prerequisites, many=True)
     return Response(serializer.data)
 
 
-
 @api_view(['GET'])
-def getPrereqs(request, course_id):
+def getPrereqs(request, course_code):
     """ Return prerequisites in logical form of given course
         :param: course_id - course ID in the form [A-Z]{2,4}\d{2,3}
         :return: the prerequisites for the given course
@@ -129,25 +120,19 @@ def getPrereqs(request, course_id):
     req = requests.get(f"{env('DATABASE_URL')}/getCourse/{course_code}")
     if (req == None):
         return
-
     course = req.json()
     course_id = course.get("id")
-
-
     # get all the prerequisite records with course_id matching with the above course_id
     prerequisites = Prerequisite.objects.filter(course_id_id=course_id) # SELECT * FROM prerequisite WHERE course_id=course_id
-
     # create a list of lists, where each inner list is a disjuction. Each entry is a course_code
     data = []
     current_char = None
     current_inner_list_index = 0
     for prerequisite1 in prerequisites:
         prerequisite = PrerequisiteSerializer(prerequisite1)
-
         prerequisite_courses = Course.objects.get(id=prerequisite['course_id_prereq'].value)
         course_serializer = CourseSerializer(prerequisite_courses)
         serialized_course_code = course_serializer["course_code"].value
-
         if current_char == None:
             current_char = prerequisite['conjunction_expression'].value
             data.append([serialized_course_code])
@@ -158,29 +143,33 @@ def getPrereqs(request, course_id):
                 data.append([serialized_course_code])
                 current_inner_list_index = current_inner_list_index + 1
                 current_char = prerequisite['conjunction_expression'].value
-
     # convert python list into json
     data_json = json.dumps(data)
-
     return Response(data_json)
 
-
-
+    
 @api_view(['GET'])
-def getDependants(request, course_id):
+def getDependants(request, course_code):
     """ Return dependant courses of given course
         :param: course_id - course ID in the form [A-Z]{2,4}\d{2,3}
         :return: all dependant courses of the given course
     """
-    # ** need to set up dependant table **
-    courses = Course.objects.all()
-    serializer = CourseSerializer(courses, many=True)
-    return Response(serializer.data)
-
-
-
-
-
-
-
-
+    # find course in course table corresponding to course_code
+    req = requests.get(f"{env('DATABASE_URL')}/getCourse/{course_code}")
+    if (req == None):
+        return
+    course = req.json()
+    course_id = course.get("id")
+    # get a querySet that contains all dependant records such that course_id=course_id
+    dependancy_records = Dependency.objects.filter(course_id_id=course_id) # SELECT * FROM dependancy WHERE course_id=course_id
+    # for each record, use the course_id_depend to find its course_code in course table
+    data = []
+    for record in dependancy_records:
+        serialized_record = DependencySerializer(record)
+        dependant_course = Course.objects.get(id=serialized_record['course_id_depend'].value) # get course object corresponding to 
+        dependant_serializer = CourseSerializer(dependant_course)
+        dependant_course_code = dependant_serializer["course_code"].value
+        data.append(dependant_course_code)
+    # convert python list into json
+    data_json = json.dumps(data)
+    return Response(data_json)
