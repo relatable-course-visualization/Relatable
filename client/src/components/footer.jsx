e2e53e5d2f9c06b88f43baf25dc7d7874ff26999
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2023-04-02 15:05:19 -0600

    add logo to footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,5 +1,7 @@
 import { Button, Container, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import RelatableLogo from "../images/relatable_full_logo.png";
+import "../stylings/footer.css";
 
 // MUI's makeStyles hooks is used to add CSS
 const useStyles = makeStyles({
@@ -41,6 +43,10 @@ const useStyles = makeStyles({
       alignItems: "center",
     },
   },
+
+  // footerContainer: {
+  //   textAlign: "center",
+  // },
 });
 
 function Footer() {
@@ -48,7 +54,8 @@ function Footer() {
 
   return (
     <footer className={classes.footer}>
-      <Container minWidth="md">
+      <Container minWidth="md" className={classes.footerContainer}>
+        <img src={RelatableLogo} alt="Relatable Logo" id="logo" />
         <Grid
           alignItems="center"
           container
