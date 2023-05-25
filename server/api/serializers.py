441e120722a3bc36559ff9601d00a9e953f13c7d
Fraser McLeod <fdm360@mail.usask.ca>
2023-05-25 16:50:59 -0600

    comment out unused code

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -12,27 +12,27 @@ class Course2023Serializer(serializers.ModelSerializer):
 
 # Old Serializers:
 ############################################################################################################
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = ['id', 'course_code', 'name', 'description', 'restrictions', 'hyperlink', 'num_credits']
+# class CourseSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Course
+#         fields = ['id', 'course_code', 'name', 'description', 'restrictions', 'hyperlink', 'num_credits']
 
-class PrerequisiteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Prerequisite
-        fields = ['id', 'course_id', 'conjunction_expression', 'course_id_prereq']
+# class PrerequisiteSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Prerequisite
+#         fields = ['id', 'course_id', 'conjunction_expression', 'course_id_prereq']
 
-class DependencySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Dependency
-        fields = ['id', 'course_id', 'course_id_depend']
+# class DependencySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Dependency
+#         fields = ['id', 'course_id', 'course_id_depend']
 
-class TestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TestDB
-        fields = ['name', 'favColour', 'favNumber']
+# class TestSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = TestDB
+#         fields = ['name', 'favColour', 'favNumber']
 
-class CoursePreqInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoursePreqInfo
-        fields = ['course_code', 'name', 'prereqString']
+# class CoursePreqInfoSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CoursePreqInfo
+#         fields = ['course_code', 'name', 'prereqString']
--- server/api/urls.py
+++ server/api/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     # POST Endpoints
     path('postCourse2023', views.postCourse2023),
-    
+
     # GET Endpoints
 
 
@@ -18,20 +18,20 @@ urlpatterns = [
 # Old Endpoints:
 ######################################################################################
     # GET endpoints
-    path('courses', views.getAllCourses), 
-    path('getCoursePrereqInfo', views.getCoursePrereqInfo),
-    re_path(r'^getCourse/([A-za-z]{2,4}\d{2,3})$', views.getCourse), #path like /getCourse/CMPT141/
-    re_path(r'^getPrereqs/([A-za-z]{2,4}\d{2,3})$', views.getPrereqs), 
-    re_path(r'^getDependants/([A-za-z]{2,4}\d{2,3})$', views.getDependants),
-    path('prerequisites', views.getAllPrereqs),
+    # path('courses', views.getAllCourses), 
+    # path('getCoursePrereqInfo', views.getCoursePrereqInfo),
+    # re_path(r'^getCourse/([A-za-z]{2,4}\d{2,3})$', views.getCourse), #path like /getCourse/CMPT141/
+    # re_path(r'^getPrereqs/([A-za-z]{2,4}\d{2,3})$', views.getPrereqs), 
+    # re_path(r'^getDependants/([A-za-z]{2,4}\d{2,3})$', views.getDependants),
+    # path('prerequisites', views.getAllPrereqs),
 
-    # POST endpoints
-    path('postTest', views.postTest),
-    path('postCoursePrereqInfo', views.postCoursePrereqInfo),
-    path('postCourse', views.postCourse),
-    path('postPrerequisite', views.postPrerequisite),
-    path('postDependency', views.postDependency),
+    # # POST endpoints
+    # path('postTest', views.postTest),
+    # path('postCoursePrereqInfo', views.postCoursePrereqInfo),
+    # path('postCourse', views.postCourse),
+    # path('postPrerequisite', views.postPrerequisite),
+    # path('postDependency', views.postDependency),
 
-    # PUT endpoints
-    path('updateCourse', views.updateCourse)
+    # # PUT endpoints
+    # path('updateCourse', views.updateCourse)
 ]
