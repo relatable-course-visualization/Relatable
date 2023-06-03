8b0817a05e2fde9cf9c6dabe812bbcbcadbb04b5
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-06-03 16:03:31 -0600

    adjust width and height of course card

--- client/src/components/courseCard.jsx
+++ client/src/components/courseCard.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-export default function CourseCard() {
+export default function CourseCard(props) {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -17,26 +17,32 @@ export default function CourseCard() {
   return (
     <Card
       sx={{
-        maxWidth: 345,
-        height: isHovered ? 'auto' : 150,
+        '@media (max-width: 768px)': {
+          width: isHovered ? '90%' : '65%',
+        },
         transition: 'height 0.3s ease-in-out',
+        height: isHovered ? 'auto' : 150,
+        width: isHovered ? 750 : 450,
       }}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       className="course-card"
     >
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Course name
-        </Typography>
-        <Typography variant="body2" color="text.secondary" noWrap={!isHovered}>
-          Principles and techniques for developing software combined with the
-          practical experience of creating a mid-size software system as a
-          member of a software development team. Includes: teamwork; projects,
-          planning and process; users and requirements; use cases; modeling;
-          quality; software architecture; testing; GUI design, design
-          principles, patterns and implementation; ethics; professionalism.
-        </Typography>
+      <CardContent className="course-card-content">
+        <div className="course-title">
+          <p>Course name {props.name}</p>
+        </div>
+        <div noWrap={!isHovered} className="course-body">
+          <p>
+            Principles and techniques for developing software combined with the
+            practical experience of creating a mid-size software system as a
+            member of a software development team. Includes: teamwork; projects,
+            planning and process; users and requirements; use cases; modeling;
+            quality; software architecture; testing; GUI design, design
+            principles, patterns and implementation; ethics; professionalism.
+            {props.body}
+          </p>
+        </div>
       </CardContent>
     </Card>
   );
