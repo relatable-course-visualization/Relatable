ff443f3bfbe300c48a381d228f6ff7fa67024e53
Fraser McLeod <fdm360@mail.usask.ca>
2023-06-26 17:55:49 -0600

    Create styling to make footer placement and sizing easier

--- client/src/pages/aboutUsPage.jsx
+++ client/src/pages/aboutUsPage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import "../stylings/home.css";
+import "../stylings/aboutUsPage.css"
 import Footer from "../components/footer";
 import AboutUs from '../components/aboutUs';
 
 const AboutUsPage = () => {
   return (
     <div id="aboutUsPage">
-      <AboutUs />
-      {/* Add footer back in later - will need to do some more work to 
-      have it properly formated */}
-      {/* <Footer /> */}
+      <div id="content-wrap">
+        <AboutUs/>
+        {/* <Footer/> */}
+      </div>
+      <Footer/>
     </div>
 
   );
