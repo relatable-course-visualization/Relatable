f5830fd6f3b024d2c52de29030ec0b10174d57c1
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2023-08-07 11:15:41 -0600

    configure server deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 
 
 # Application definition
